bfdf014eee871dd4de09476860991f133e3c52d6
Kondemahesh <99584470+Kondemahesh@users.noreply.github.com>
2025-07-24 17:31:36 +0530

    Delete modules directory

--- modules/ec2_instance/output.tf
+++
@@ -1,8 +0,0 @@
-output "role_name" {
-  value = aws_iam_role.this.name
-}
-
-output "public_ip" {
-   value = "aws_instance.web_server.public_ip" 
-}
-
--- modules/vpc/output.tf
+++
@@ -1,9 +0,0 @@
-output "public_subnet_ids" {
-  value = [for subnet in aws_subnet.public : subnet.id]
-}
-
-
-output "vpc_id" {
-  value  =  aws_vpc.this.id
-
-}
